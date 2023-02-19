fdc085223c14b824acd34c584d50e15f4627fc24
Guyutongxue <guyutongxue@163.com>
2023-02-19 21:17:05 +0800

    ok

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -7,37 +7,37 @@ export default defineConfig({
     themeConfig: {
         sidebar: [
             {
-                text: "第一课",
+                text: "第一课：Lisp 介绍",
                 link: "/lesson1"
             },
             {
-                text: "第二课",
+                text: "第二课：组合式",
                 link: "/lesson2"
             },
             {
-                text: "第三课",
+                text: "第三课：命名",
                 link: "/lesson3"
             },
             {
-                text: "第四课",
+                text: "第四课：特殊形式",
                 link: "/lesson4"
             },
             {
-                text: "第五课",
+                text: "第五课：列表",
                 link: "/lesson5"
             },
             {
-                text: "第六课",
+                text: "第六课：映射",
                 link: "/lesson6"
             },
-            // {
-            //     text: "第七课",
-            //     link: "/lesson7"
-            // },
-            // {
-            //     text: "第八课",
-            //     link: "/lesson8"
-            // },
+            {
+                text: "第七课：对子",
+                link: "/lesson7"
+            },
+            {
+                text: "结语",
+                link: "/episode"
+            },
         ]
     }
 });
--- docs/index.md
+++ docs/index.md
@@ -2,10 +2,6 @@
 
 这份文档试图在最短的时间内让你了解 Lisp 编程语言，从而可以用它编写一些简单的算法。如果你已经准备好了，就直接进入 [第一课](./lesson1.md) 吧。
 
-如果你还想多了解一些 Lisp，我建议你阅读《计算机程序的构造和解释（第 2 版）》（*Structure and Interpretation of Computer Programs*，SICP）。有中文版，翻译得还不错。
+如果你想正式地而非快餐式地学习 Lisp，我建议你阅读《计算机程序的构造和解释（第 2 版）》（*Structure and Interpretation of Computer Programs*，SICP）。有中文版，翻译得还不错。
 
-::: details
-
-创意来自[正则表达式 30 分钟入门教程](https://deerchao.cn/tutorials/regex/regex.htm)。如果你看过这个的话，就知道“30 分钟”是标题党，忽悠人用的——为的是让你有信心学下去。
-
-:::
+> 本文档创意来自[正则表达式 30 分钟入门教程](https://deerchao.cn/tutorials/regex/regex.htm)。
